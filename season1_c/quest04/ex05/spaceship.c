dbf8c31981f047864e19730cee3e3c6a28e1ae19
Akinade Mayowa <akinade.mayowa@gmail.com>
2022-08-25 21:12:35 +0100

    my spaceship.c done

--- season1_c/quest04/ex05/spaceship.c
+++ season1_c/quest04/ex05/spaceship.c
@@ -4,14 +4,17 @@
 
 char* my_spaceship(char* param_1){
     // let r = right; left = l, etc
+    //char buffer[];
     int x_coord = 0;
     int y_coord = 0;
     char cur_pos = 'u';
     char* direction;
     direction = (char*)malloc(strlen(param_1)*sizeof(char));
     //printf("%d\n", size);
+    char* result;
+    result = (char*)malloc(500*sizeof(char));
 
-    //direction = "up";
+    direction = "up";
     int size = sizeof(param_1)/sizeof(param_1[0]);
     for (int i=0; i<size; i++) {
         if(param_1[i] == 'R') {
@@ -54,14 +57,16 @@ char* my_spaceship(char* param_1){
             }
         }
     }
-    printf("%c\n", cur_pos);
-    printf("%s\n", direction);
-    printf("{x: %d, y: %d, direction: %s}", x_coord, y_coord, direction);
+    int c = snprintf(result, 100, "{x: %d, y: %d, direction: %s}", x_coord, y_coord, direction);
+    return result;
+    // printf("%c\n", cur_pos);
+    // printf("%s\n", direction);
+    // printf("{x: %d, y: %d, direction: %s}", x_coord, y_coord, direction);
     //return `{x: ${x_coord}, y: ${y_coord}, direction: '${cur_pos}'}`
 }
 
 char main() {
     char* dir; // = "RAJ";
-    dir = "RAALALL";
-    my_spaceship(dir);
+    dir = "RAALALL"; //RAALALL
+    printf("%s\n", my_spaceship(dir));
 }
