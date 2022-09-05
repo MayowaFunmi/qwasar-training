f804b4965bac60d97d79374a3d96afa25ab23a4f
Akinade Mayowa <akinade.mayowa@gmail.com>
2022-09-05 17:33:52 +0100

    added my_ngram group project

---
+++ season1_c/ngram/my_ngram_test.c
@@ -0,0 +1,39 @@
+﻿#include <stdio.h>
+#include <string.h>
+
+void printfrequencyofcharacters(char *s){
+	int i,j,count=0,n;
+	n=strlen(s); 
+    
+	printf(" frequency count character in string:\n");
+	for(i=0;i<n;i++){
+		count=1;
+    if(s[i]){
+    	if (s[i] != NULL){
+ 		  	for(j=i+1;j<n;j++){
+ 		  		if(s[i]==s[j]){
+ 		  			count++;
+ 		  			s[j]='\0';
+	     		}
+	    	}
+	    	printf(" '%c' = %d \n",s[i],count);
+	    }
+	  }
+	}
+}
+
+
+int main()
+{
+
+    char s[1000];
+  
+    printf("Enter  the string : ");
+    gets(s);
+    printfrequencyofcharacters(s);
+ 
+	return 0;
+ 
+     
+     
+}
---
+++ season1_c/ngram/ngram_group.c
@@ -0,0 +1,32 @@
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int main( int argc, char argv[] )  {
+
+    char* sent = "aaaabbbcadaad";
+    char* alph_store = malloc(100);
+ 
+    //iterate through the string
+    for(int i=0; sent[i]!='\0'; i++){
+
+        //take the current character and start iterating from the index
+
+        if(strchr(alph_store, sent[i]) == NULL){
+
+             int counter = 1;
+            for(int j=i+1; sent[j] !='\0'; j++){
+
+                if(sent[j]==sent[i]){
+                    ++counter;
+                }
+            }
+
+            // if (strchr(alph_store, sent[i]) == NULL) 
+            printf("%c:%d\n", sent[i], counter); 
+            alph_store[i] = sent[i];
+
+        }
+    }
+
+}
